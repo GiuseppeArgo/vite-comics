b7a6c7f1061483c86ac12524ac70f1cabb6c9dd0
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-05-02 18:53:13 +0200

    feat: project header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,98 @@
 <script>
 export default {
-
+    data() {
+    return {
+      menu: [
+        {
+          title: "CHARACTERS",
+        },
+        {
+          title: "COMICS",
+        },
+        {
+          title: "MOVIES",
+        },
+        {
+          title: "TV",
+        },
+        {
+          title: "GAMES",
+        },
+        {
+          title: "COLLECTIBLES",
+        },
+        {
+          title: "VIDEOS",
+        },
+        {
+          title: "FANS",
+        },
+        {
+          title: "NEWS",
+        },
+        {
+          title: "SHOP",
+        },
+      ],
+    };
+  },
 }
 </script>
 
 <template>
-    <div>
-        <h1>ciao</h1>
-    </div>
+    <header>
+        <div class="header-img">
+            <img src="../assets/img/dc-logo.png" alt="Logo DC">
+        </div>
+        <ul class="header-nav">
+            <li v-for="item in menu">
+                <a href="">{{ item.title }}</a>
+            </li>
+        </ul>
+    </header>
 </template>
 
 <style lang="scss" scoped>
+@use "../style/partials/variable" as *;
+@use "../style/partials/mixin" as *;
 
+header{
+    @include flex(row,space-between,center);
+    padding: 20px;
+    margin-top: 30px;
+    height: 100px;
+    background-color: $white;
+
+    .header-img{
+    @include flex(row,center,center);
+    flex-grow: 1;
+    img{
+        width: 17%;
+        margin-left: 100px;
+    }
+
+}
+.header-nav{
+    @include flex(row,end,center);
+    gap: 20px;
+    width: 60%;
+    margin-right: 300px;
+    li{
+        padding: 40px 0;
+        font-family: sans-serif;
+        font-size: 0.8rem;
+        font-weight: 700;
+        border-bottom: 5px solid transparent;
+        &:hover{
+            border-bottom: 5px solid $blue;
+            color: $blue;
+        }
+    }
+    a{
+        text-decoration: none;
+        color: $black;
+    }
+}
+}
 
 </style>
