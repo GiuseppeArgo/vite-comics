11f0e953e7e14a5bd0dd13ccdd14a630c47eb1ea
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-05-05 16:10:41 +0200

    feat: completed card section in AppMain and minor fixes

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,7 +1,11 @@
 <script>
+import AppCard from './AppCard.vue';
+import ProductCard from './AppCard.vue';
+
 export default {
     data() {
         return {
+            
             comics: [
                 {
                 title: "Characters",
@@ -87,13 +91,87 @@ export default {
                 {
                 title: "DC Power Visa",
                 },
+            ],
+
+            products: [
+                {
+                "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                "price": "$19.99",
+                "series": "Action Comics",
+                "type": "comic book",
+                },
+                {
+                "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                "price": "$3.99",
+                "series": "American Vampire 1976",
+                "type": "comic book",
+                },
+                {
+                "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                "price": "$16.99",
+                "series": "Aquaman",
+                "type": "graphic novel",
+                },
+                {
+                "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                "price": "$2.99",
+                "series": "Batgirl",
+                "type": "comic book",
+                },
+                {
+                "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                "price": "$3.99",
+                "series": "Batman",
+                "type": "comic book",
+                },
+                {
+                "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                "price": "$2.99",
+                "series": "Batman Beyond",
+                "type": "comic book",
+                },
+                {
+                "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                "price": "$3.99",
+                "series": "Batman/Superman",
+                "type": "comic book",
+                },
+                {
+                "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                "price": "$4.99",
+                "series": "Batman/Superman Annual",
+                "type": "comic book",
+                },
+                {
+                "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                "price": "$5.99",
+                "series": "Batman: The Joker War Zone",
+                "type": "comic book",
+                },
+                {
+                "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                "price": "$6.99",
+                "series": "Batman: Three Jokers",
+                "type": "comic book",
+                },
+                {
+                "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                "price": "$4.99",
+                "series": "Batman: White Knight Presents: Harley Quinn",
+                "type": "comic book",
+                },
+                {
+                "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                "price": "$16.99",
+                "series": "Catwoman",
+                "type": "graphic novel",
+                },
             ]
         };
     },
-    props: {
-        elemImage: String,
-        elemSeries: String,
-        elemType: String,
+    
+    components: {
+        AppCard
     },
 }
 </script>
@@ -101,10 +179,10 @@ export default {
 <template>
     <section>
         <div class="upper-main">
-            <div class="card">
-                <img :src="elemImage"
-                :class="elemType === 'comic book' ? 'comic-book' : 'graphic-novel'">
-                <span>{{ elemSeries }}</span>
+            <div class="row">
+                <div class="col" v-for="element in products">
+                    <AppCard :thumb="element.thumb" :series="element.series"/>
+                </div>
             </div>
         </div>
         <div class="icon-main">
@@ -160,11 +238,19 @@ section {
     .upper-main {
         @include flex(row,start,center);
         background-color: $black;
-        height: 15vh;
-        padding-left: 200px;
         font-size: 2rem;
     }
-        
+    
+    .row {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        .col {
+                width: 15%;
+                margin-bottom: 2em;
+            }
+    }
+
     .icon-main {
         @include flex(row,start,center);
         padding-left: 200px;
@@ -204,37 +290,9 @@ section {
         img {
             width: 500px;
             position: absolute;
-            top: 400px;
             right: 300px;
+            top:1090px
         }
     }
-    
-    .card {
-    @include flex(column,space-between,center);
-    position: relative;
-    z-index: 10;
-    color: $white;
-    width: calc(100% / 6 - 50px / 6);
-    img {
-        width: 10vw;
-        height: 26vh;
-        margin-bottom: 10px;
-    }
-    .comic-book{
-        border:1px solid red;
-    }
-    .graphic-novel{
-        border:1px solid green;
-    }
-    span{
-        display: inline-block;
-        height: 30px;
-        font-family: sans-serif;
-        font-size: 10px;
-    }
-    span,img{
-        align-self: flex-start;
-    }
-}
 }
 </style>
